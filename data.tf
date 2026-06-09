0f28b33c991e7a976008cef1120e07e953ba6627
manju230 <111728745+manju230@users.noreply.github.com>
2026-06-09 17:14:08 +0530

    Update data.tf

--- data.tf
+++ data.tf
@@ -1,15 +1,20 @@
-data "aws_ami" "ubuntu" {
+data "aws_ami" "amazon_linux" {
   most_recent = true
+
+  owners = ["amazon"]
 
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+    values = ["al2023-ami-*-x86_64"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
   }
 
   filter {
     name   = "virtualization-type"
     values = ["hvm"]
   }
-
-  owners = ["099720109477"] # Canonical
 }
