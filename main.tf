3b411601098985e59d42901cd4e42dcf5cdefc8d
manju230 <111728745+manju230@users.noreply.github.com>
2026-06-09 17:18:33 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "example" {
         count = 2
   ami           = data.aws_ami.amazon_linux.id
+subnet_id = "subnet-0ba43e54dc640ac9d"
   instance_type = "t3.micro"
 
   tags = {
